454253f73ceccda353b54ea1f18fdac120f4fe65
valentin-vegee <valentinvegee132@gmail.com>
2023-04-24 15:23:07 +0200

    header niv 2

--- view/header.php
+++ view/header.php
@@ -4,18 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../assets/css/style3.css" type="text/css" rel="stylesheet" />
     <title>Document</title>
 </head>
-<header style="background-color: blue;">
-  <div class="profile">
-    <img src="votre_photo.jpg" alt="Photo de profil" class="profile-photo">
-    <span class="profile-text">Profil</span>
+
+<header class="header">
+  <div class="header-profile">
+    <div class="header-profile-photo-container">
+      <img src="votre_photo.jpg" alt="Photo de profil" class="header-profile-photo">
+    </div>
+    <span class="header-profile-text">Profil</span>
   </div>
-  <a href="home.html"><img src="logo.png" alt="Logo" class="logo"></a>
-  <div class="buttons">
-    <button class="button">Bouton 1</button>
-    <button class="button">Bouton 2</button>
-    <button class="button">Bouton 3</button>
+  <a href="home.html"><img src="logo.png" alt="Logo" class="header-logo"></a>
+  <div class="header-buttons">
+    <button class="header-button">Bouton 1</button>
+    <button class="header-button">Bouton 2</button>
+    <button class="header-button">Bouton 3</button>
+  </div>
+  <div class="header-shapes">
+    <div class="header-shape header-shape-1"></div>
+    <div class="header-shape header-shape-2"></div>
   </div>
 </header>
+
 </html>
